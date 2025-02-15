c2ccf1bb99da1ea70cff626f378f13741c4b511b
nithin-sudo <sathramnithin@gmail.com>
2025-02-15 00:24:56 -0500

    added comments to the code.

--- sumofevenoddarray.py
+++ sumofevenoddarray.py
@@ -1,10 +1,13 @@
 # Return sum of second largest element from even position and second smallest element from odd position
 
+# initialized an array
 nums = [10,45,11,1,22,2,4]
 
 index = 0
 evens = list()
 odds = list()
+
+# seperating evens and odds into their respective arrays
 for num in nums :
     if(index % 2 == 0):
         evens.append(num)
@@ -13,6 +16,11 @@ for num in nums :
     index = index + 1
 
 # bubble sort
+# used bubble sort to sort each array, easy sorting algorithm but it is not fast enough.
+
+''' first for loop, iterates through the whole array once, inside loop actually takes care of
+comparing each element with adjacent element and swaps accordingly.'''
+
 for i in range(0,len(evens)- 1):
     for  j in range(0,len(evens)-i-1):
         if evens[j] > evens[j+1]:
