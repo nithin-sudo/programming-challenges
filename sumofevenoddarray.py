384ebae48228c706d7e683f01b0b386d55b40f16
nithin-sudo <sathramnithin@gmail.com>
2025-02-15 00:20:23 -0500

    first commit

---
+++ sumofevenoddarray.py
@@ -0,0 +1,28 @@
+# Return sum of second largest element from even position and second smallest element from odd position
+
+nums = [10,45,11,1,22,2,4]
+
+index = 0
+evens = list()
+odds = list()
+for num in nums :
+    if(index % 2 == 0):
+        evens.append(num)
+    else:
+        odds.append(num)
+    index = index + 1
+
+# bubble sort
+for i in range(0,len(evens)- 1):
+    for  j in range(0,len(evens)-i-1):
+        if evens[j] > evens[j+1]:
+            evens[j],evens[j+1] = evens[j+1],evens[j]
+
+for i in range(0,len(odds)-1):
+    for j in range(0,len(odds)-i-1):
+        if odds[j] > odds[j+1]:
+            odds[j],odds[j+1] = odds[j+1],odds[j]
+
+sum_of_elements = evens[len(evens) - 2] + odds[1]
+
+print("sum of second largest element from even position and second smallest element from odd position: ", sum_of_elements)
